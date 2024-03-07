dc4c9ea2aee0a88b5ebcc9dea329da7448503076
Miguel-Martin-Quintana <miguelmartinpracticas@gmail.com>
2024-03-07 20:29:59 +0100

    Agregada nueva funcionalidad

---
+++ Funcionalidad.c
@@ -0,0 +1,13 @@
+/*Condicionales 1*/
+#include <stdio.h>
+
+void main(){
+    int numero = 4;
+    if (numero % 2 == 0 )
+    {
+        printf("El numero %d es un numero par.",numero);
+    }
+    else{
+        printf("El numero %d es un numero impar.",numero);
+    }
+}
